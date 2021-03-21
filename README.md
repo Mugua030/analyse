f4bd632e638194c464ac81d19092f370c763e8bd
Mugua030 <email.mugua@gmail.com>
2021-03-21 15:17:20 +0800

    list

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 extract biz logic info from big log 
 
 ### analyse info from Big file 
+
+
+	1.extrac
+	2.export report
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Begin...");
+
+    // fetch info that matched to local file
+    
+
+    // extract info that biz logic matched
+    
+}
